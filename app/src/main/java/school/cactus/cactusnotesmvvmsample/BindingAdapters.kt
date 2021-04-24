3fb7766e31318f89823bee692e8eb4702ec7a552
simgesengun <simgesngn@gmail.com>
2021-04-24 21:48:48 +0300

    Fixed Resources.NotFoundException

--- app/src/main/java/school/cactus/cactusnotesmvvmsample/BindingAdapters.kt
+++ app/src/main/java/school/cactus/cactusnotesmvvmsample/BindingAdapters.kt
@@ -4,7 +4,7 @@ import androidx.databinding.BindingAdapter
 import com.google.android.material.textfield.TextInputLayout
 
 @BindingAdapter("android:error")
-fun setError(textInputLayout: TextInputLayout,  error: String?) {
+fun setError(textInputLayout: TextInputLayout, error: String?) {
     textInputLayout.error = error
     textInputLayout.isErrorEnabled = error != null
 }
